7baf5abd80c9a2de26f972b1a9a82ec7f996f93e
= <=>
2019-04-09 13:48:34 +0530

    fixed getting logs and added listing all pods

--- app.py
+++ app.py
@@ -17,7 +17,11 @@ def index():
 
         for line in iter(proc.stdout.readline,''):
             time.sleep(0.1)                           # Don't need this just shows the text streaming
-            yield str(line).rstrip() + '<br/>\n'
+            yield str(line).strip() + '<br/>\n'
+    # text/html and text/plain seem to work
+    return flask.Response(inner(podname), mimetype='text/html')  
 
-    return flask.Response(inner(podname), mimetype='text/html')  # text/html is required for most browsers to show th$
-
+@app.route('/pods')
+def pods():
+    pods = subprocess.check_output("kubectl get pods --all-namespaces", shell=True)
+    return  flask.Response(pods, mimetype='text/plain')
