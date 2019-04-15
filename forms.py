2e26ef9e94f9a9ebdfb4acc69749b2f933164276
govind somalal <govindsomalal7@gmail.com>
2019-04-15 16:26:21 +0530

    form for building image

---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class BuildForm(FlaskForm):
+    #password = PasswordField('Password', validators=[DataRequired()])
+    #remember_me = BooleanField('Remember Me')
+
+    version           = StringField('version', validators           = [DataRequired()])
+    github_user       = StringField('github _user', validators       = [DataRequired()])
+    github_revision   = StringField('github_revision', validators   = [DataRequired()])
+    github_repo       = StringField('github_repo', validators       = [DataRequired()])
+    docker_user       = StringField('docker_user', validators       = [DataRequired()])
+    train_name        = StringField('train_name', validators        = [DataRequired()])
+    docker_image_name = StringField('docker_image_name', validators = [DataRequired()])
+    build             = SubmitField('build')
