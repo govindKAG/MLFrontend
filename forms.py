bffc45bfc853dd838d3d18a5f16c0d938db826f6
govind somalal <govindsomalal7@gmail.com>
2019-04-16 12:58:23 +0530

    added class for train ui

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 class BuildForm(FlaskForm):
@@ -14,3 +14,15 @@ class BuildForm(FlaskForm):
     train_name        = StringField('train_name', validators        = [DataRequired()])
     docker_image_name = StringField('docker_image_name', validators = [DataRequired()])
     build             = SubmitField('build')
+
+class TrainForm(FlaskForm):
+    #password = PasswordField('Password', validators=[DataRequired()])
+    #remember_me = BooleanField('Remember Me')
+
+    version           = StringField('version', validators           = [DataRequired()])
+    docker_user       = StringField('docker_user', validators       = [DataRequired()])
+    train_name        = StringField('train_name', validators        = [DataRequired()])
+    docker_image_name = StringField('docker_image_name', validators = [DataRequired()])
+    command           = StringField('command', validators           = [DataRequired()])
+    args              = TextAreaField('args', validators            = [DataRequired()])
+    train             = SubmitField('train')
