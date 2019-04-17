85099b611a890dd6ad78dd60b1b5a80251613233
govind somalal <govindsomalal7@gmail.com>
2019-04-17 09:43:01 +0530

    added method to pull out latest training pod name

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 import shlex
+import subprocess
+import time
 from ruamel.yaml import YAML 
 
 def generate_yaml(input_file_name, output_file_name, args):
@@ -25,3 +27,15 @@ def generate_yaml(input_file_name, output_file_name, args):
         #yaml.dump(ogfile, f, default_flow_style=False)
         yaml.dump(ogfile, f) 
 
+def get_train_pod_name(job_name):
+    time.sleep(5)
+    output = subprocess.check_output("kubectl get pods", shell=True)
+    output = bytes.decode(output)
+    print(output)
+    for i in output.split():
+        if job_name in i:
+            print(i)
+            return i
+
+#if __name__ == '__main__':
+#    get_train_pod_name('emote-trainb17')
