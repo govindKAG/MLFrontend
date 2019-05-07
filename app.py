a93e53ec6b29e36261ecc2e7f0fd6378ecc48b64
govind somalal <govindsomalal7@gmail.com>
2019-05-07 13:06:25 +0530

    added backend and routes for simple demo

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from json2html import *
 from forms import BuildForm
 from forms import TrainForm
 from forms import DeployForm
+from forms import DemoForm
 from util import generate_yaml
 from util import get_train_pod_name
 
@@ -195,3 +196,29 @@ def list_models():
     #return json.dumps(statuses)
     return render_template('deploytables.html', statuses=statuses)
     #return  flask.Response(pods, mimetype='text/plain')
+
+@app.route('/demo_app')
+def demo_app():
+    query    = request.args.get('query')
+    result   = subprocess.check_output('''curl -X POST -H "Content-Type: application/json" -d "{\\"data\\":{\\"ndarray\\":[[\\"'''+query+ '''}\\"]]}}" http://10.71.3.3:32389/seldon/emote-classifier/api/v0.1/predictions ''',shell = True)
+    result   = json.loads(result)
+    emotion  = 'positive'
+    color    = 'bg-green'
+    if result['data']['ndarray'][0][0] == 'pos':
+        emotion = 'positive'
+    else :
+        emotion = 'negative'
+        color   = 'bg-red'
+    return render_template('emotion.html', color = color)
+    #return render_template('deploytables.html', statuses=statuses)
+    #return  flask.Response(pods, mimetype='text/plain')
+
+@app.route('/demoui', methods=['GET', 'POST'])
+def demoui():
+    form = DemoForm()
+    if form.validate_on_submit():
+        #print(request.form.to_dict())
+        args = copy.deepcopy(request.form.to_dict())
+        del args['csrf_token']
+        return redirect(url_for('demo_app',**args))
+    return render_template('demo.html', title='nexo', form=form)
